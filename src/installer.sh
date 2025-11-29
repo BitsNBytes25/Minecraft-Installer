0e31e46988d18318d370e86fd7f23826d76bd25b
Charlie Powell <cdp1337@veraciousnetwork.com>
2025-11-29 13:57:14 -0500

    Prepping for release

--- src/installer.sh
+++ src/installer.sh
@@ -37,7 +37,7 @@
 ############################################
 
 # Name of the game (used to create the directory)
-INSTALLER_VERSION="v20251127~DEV"
+INSTALLER_VERSION="v20251129"
 GAME="Minecraft"
 GAME_DESC="Minecraft Dedicated Server"
 REPO="BitsNBytes25/Minecraft-Installer"
